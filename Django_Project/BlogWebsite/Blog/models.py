997e162c4ffba88bb8961facb72c8908ae9baf0d
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-06-30 22:05:02 +0530

    worked on forms.py file

--- Django_Project/BlogWebsite/Blog/models.py
+++ Django_Project/BlogWebsite/Blog/models.py
@@ -24,7 +24,7 @@ class Post(models.Model):
         return self.title
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post',related_name=comments)
+    post = models.ForeignKey('Blog.Post',related_name=comments)
     author = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
